fd5d4d64b31dc0f16796c87ff9d7817822b84a4b
joe <kinjoyuji@gmail.com>
2018-08-25 15:02:51 +0900

    migration!

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id              :bigint(8)        not null, primary key
+#  blocked         :boolean          default(FALSE), not null
+#  followable_type :string(255)      not null
+#  follower_type   :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  followable_id   :bigint(8)        not null
+#  follower_id     :bigint(8)        not null
+#
+# Indexes
+#
+#  fk_followables                                      (followable_id,followable_type)
+#  fk_follows                                          (follower_id,follower_type)
+#  index_follows_on_followable_type_and_followable_id  (followable_type,followable_id)
+#  index_follows_on_follower_type_and_follower_id      (follower_type,follower_id)
+#
+
 class Follow < ActiveRecord::Base
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
--- db/structure.sql
+++ db/structure.sql
@@ -71,12 +71,87 @@ CREATE TABLE `delayed_jobs` (
   KEY `delayed_jobs_priority` (`priority`,`run_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `follows`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `follows` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `followable_type` varchar(255) NOT NULL,
+  `followable_id` bigint(20) NOT NULL,
+  `follower_type` varchar(255) NOT NULL,
+  `follower_id` bigint(20) NOT NULL,
+  `blocked` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_follows_on_followable_type_and_followable_id` (`followable_type`,`followable_id`),
+  KEY `index_follows_on_follower_type_and_follower_id` (`follower_type`,`follower_id`),
+  KEY `fk_follows` (`follower_id`,`follower_type`),
+  KEY `fk_followables` (`followable_id`,`followable_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `schema_migrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `schema_migrations` (
   `version` varchar(255) NOT NULL,
   PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `taggings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `taggings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) DEFAULT NULL,
+  `taggable_type` varchar(255) DEFAULT NULL,
+  `taggable_id` int(11) DEFAULT NULL,
+  `tagger_type` varchar(255) DEFAULT NULL,
+  `tagger_id` int(11) DEFAULT NULL,
+  `context` varchar(128) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `taggings_idx` (`tag_id`,`taggable_id`,`taggable_type`,`context`,`tagger_id`,`tagger_type`),
+  KEY `index_taggings_on_taggable_id_and_taggable_type_and_context` (`taggable_id`,`taggable_type`,`context`),
+  KEY `index_taggings_on_tag_id` (`tag_id`),
+  KEY `index_taggings_on_taggable_id` (`taggable_id`),
+  KEY `index_taggings_on_taggable_type` (`taggable_type`),
+  KEY `index_taggings_on_tagger_id` (`tagger_id`),
+  KEY `index_taggings_on_context` (`context`),
+  KEY `index_taggings_on_tagger_id_and_tagger_type` (`tagger_id`,`tagger_type`),
+  KEY `taggings_idy` (`taggable_id`,`taggable_type`,`tagger_id`,`context`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `taggings_count` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_tags_on_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `votes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `votes` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `votable_type` varchar(255) DEFAULT NULL,
+  `votable_id` bigint(20) DEFAULT NULL,
+  `voter_type` varchar(255) DEFAULT NULL,
+  `voter_id` bigint(20) DEFAULT NULL,
+  `vote_flag` tinyint(1) DEFAULT NULL,
+  `vote_scope` varchar(255) DEFAULT NULL,
+  `vote_weight` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_votes_on_votable_type_and_votable_id` (`votable_type`,`votable_id`),
+  KEY `index_votes_on_voter_type_and_voter_id` (`voter_type`,`voter_id`),
+  KEY `index_votes_on_voter_id_and_voter_type_and_vote_scope` (`voter_id`,`voter_type`,`vote_scope`),
+  KEY `index_votes_on_votable_id_and_votable_type_and_vote_scope` (`votable_id`,`votable_type`,`vote_scope`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -91,6 +166,14 @@ CREATE TABLE `schema_migrations` (
 
 INSERT INTO `schema_migrations` (version) VALUES
 ('20180825033158'),
-('20180825033210');
+('20180825033210'),
+('20180825033235'),
+('20180825033236'),
+('20180825033237'),
+('20180825033238'),
+('20180825033239'),
+('20180825033240'),
+('20180825033256'),
+('20180825060031');
 
 
